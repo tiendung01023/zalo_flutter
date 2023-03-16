abe511f384067d5528068db122848e362a806ab7
Phạm Tiến Dũng <>
2023-03-16 17:18:53 +0700

    mod. create new example

--- example/android/app/proguard-rules.pro
+++
@@ -1,3 +0,0 @@
--keep class com.zing.zalo.**{ *; }
--keep enum com.zing.zalo.**{ *; }
--keep interface com.zing.zalo.**{ *; }
---
+++ example/android/app/src/main/kotlin/com/tiendung01023/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tiendung01023.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
