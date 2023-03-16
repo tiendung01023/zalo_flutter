2f88071f48b832a42d263af380404c31114205e0
Phạm Tiến Dũng <>
2023-03-16 17:35:12 +0700

    mod. setup zalo android

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,115 +1,288 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:zalo_flutter/zalo_flutter.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _indexReset = -1;
+  ValueKey<String> _key = const ValueKey<String>('');
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String zaloId = '2961857761415564889';
+  String zaloMessage = 'Hello';
+  String zaloLink = 'www.google.com';
+
+  String? _accessToken;
+  String? _refreshToken;
+
+  @override
+  void initState() {
+    super.initState();
+    _initZaloFlutter();
+  }
+
+  Future<void> _initZaloFlutter() async {
+    if (Platform.isAndroid) {
+      final String? hashKey = await ZaloFlutter.getHashKeyAndroid();
+      log('HashKey: $hashKey');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      key: _key,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CommonButton(
+                  text: 'clear log',
+                  onPressed: () async {
+                    _indexReset++;
+                    _key = ValueKey<String>(_indexReset.toString());
+                    setState(() {});
+                    return null;
+                  },
+                ),
+                CommonButton(
+                  text: 'login',
+                  onPressed: () async {
+                    final Map<dynamic, dynamic>? data = await ZaloFlutter.login(
+                      refreshToken: _refreshToken,
+                    );
+                    try {
+                      print("login: $data");
+                      if (data?['isSuccess'] == true) {
+                        _accessToken = data?["data"]["accessToken"] as String?;
+                        _refreshToken = data?["data"]["refreshToken"] as String?;
+                      }
+                    } catch (e) {
+                      print("login: $e");
+                    }
+                    return data;
+                  },
+                ),
+                CommonButton(
+                  text: 'logout',
+                  onPressed: () async {
+                    _accessToken = null;
+                    _refreshToken = null;
+                    await ZaloFlutter.logout();
+
+                    _indexReset++;
+                    _key = ValueKey<String>(_indexReset.toString());
+                    setState(() {});
+                    return null;
+                  },
+                ),
+                CommonButton(
+                  text: 'validateRefreshToken',
+                  onPressed: () async {
+                    final bool data = await ZaloFlutter.validateRefreshToken(
+                      refreshToken: _refreshToken ?? '',
+                    );
+                    return data.toString();
+                  },
+                ),
+                CommonButton(
+                  text: 'getUserProfile',
+                  onPressed: () async {
+                    final Map<dynamic, dynamic>? data = await ZaloFlutter.getUserProfile(
+                      accessToken: _accessToken ?? '',
+                    );
+                    return data;
+                  },
+                ),
+                CommonButton(
+                  text: 'shareMessage',
+                  onPressed: () async {
+                    // final bool result = await ZaloFlutter.shareMessage(
+                    //   link: 'https://huuksocialproduction.page.link/nHKZ',
+                    //   message: 'assadda',
+                    //   appName: 'Huuk Social',
+                    // );
+                    // return result;
+                  },
+                ),
+                // CommonButton(
+                //   text: 'OA - getUserFriendList',
+                //   onPressed: () async {
+                //     final Map<dynamic, dynamic>? data = await ZaloOAFlutter.getUserFriendList(
+                //       accessToken: _accessToken ?? '',
+                //       atOffset: 0,
+                //       count: 3,
+                //     );
+                //     return data;
+                //   },
+                // ),
+                // CommonButton(
+                //   text: 'OA - getUserInvitableFriendList',
+                //   onPressed: () async {
+                //     final Map<dynamic, dynamic>? data = await ZaloOAFlutter.getUserInvitableFriendList(
+                //       accessToken: _accessToken ?? '',
+                //       atOffset: 0,
+                //       count: 3,
+                //     );
+                //     return data;
+                //   },
+                // ),
+                // CommonButton(
+                //   text: 'OA - sendMessage',
+                //   onPressed: () async {
+                //     final Map<dynamic, dynamic>? data = await ZaloOAFlutter.sendMessage(
+                //       accessToken: _accessToken ?? '',
+                //       to: zaloId,
+                //       message: zaloMessage,
+                //       link: zaloLink,
+                //     );
+                //     return data;
+                //   },
+                // ),
+                // CommonButton(
+                //   text: 'OA - postFeed',
+                //   onPressed: () async {
+                //     final Map<dynamic, dynamic>? data = await ZaloOAFlutter.postFeed(
+                //       accessToken: _accessToken ?? '',
+                //       message: zaloMessage,
+                //       link: zaloLink,
+                //     );
+                //     return data;
+                //   },
+                // ),
+                // CommonButton(
+                //   text: 'OA - sendAppRequest',
+                //   onPressed: () async {
+                //     final Map<dynamic, dynamic>? data = await ZaloOAFlutter.sendAppRequest(
+                //       accessToken: _accessToken ?? '',
+                //       to: <String>[zaloId],
+                //       message: zaloMessage,
+                //     );
+                //     return data;
+                //   },
+                // ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class CommonButton extends StatefulWidget {
+  const CommonButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+    this.color = Colors.blue,
+  }) : super(key: key);
+
+  final String text;
+  final Future<dynamic> Function() onPressed;
+  final Color color;
+
+  @override
+  _CommonButtonState createState() => _CommonButtonState();
+}
+
+class _CommonButtonState extends State<CommonButton> {
+  String? result;
+
+  @override
+  Widget build(BuildContext context) {
+    final Widget childText = Text(
+      widget.text,
+      style: const TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+
+    final Widget button = MaterialButton(
+      minWidth: double.infinity,
+      height: 40,
+      color: widget.color,
+      padding: const EdgeInsets.all(16),
+      onPressed: () async {
+        _showLoading(context);
+        final DateTime time = DateTime.now();
+        print('[$time][commonButton] ${widget.text}');
+        final dynamic data = await widget.onPressed();
+        if (data == null) {
+          result = 'null';
+        } else if (data is String) {
+          result = data;
+        } else if (data is Map) {
+          result = jsonEncode(data);
+        }
+        setState(() {});
+        Navigator.pop(context);
+      },
+      shape: const StadiumBorder(),
+      child: childText,
+    );
+
+    Widget showResult(String? text) {
+      if (text == null) {
+        return Container();
+      }
+      String data;
+      try {
+        final Map<String, dynamic>? object = jsonDecode(text) as Map<String, dynamic>?;
+        data = const JsonEncoder.withIndent('  ').convert(object);
+      } catch (e) {
+        data = text;
+      }
+      return Text(data);
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: Column(
+        children: <Widget>[
+          button,
+          showResult(result),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _showLoading(BuildContext context) async {
+    await showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (_) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 }
